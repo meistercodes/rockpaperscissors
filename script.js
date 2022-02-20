fc180ece69717642dabb2ff00e99f5c5e07b047d
Meister <meistercodes@gmail.com>
2022-02-20 07:08:59 -0500

    rps

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let computerChoice = computerPlay();
-let playerChoice = "Rock";
+function playerPlay() {
+  let playerChoice = prompt("Rock, Paper, or Scissors?");
+  return playerChoice;
+}
 
+// Function that generates a random computer selection
 function computerPlay() {
-  let computerSelection = Math.floor(Math.random() * 3) + 1;
-  if (computerSelection === 1) {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
     return "Rock";
-  } else if (computerSelection === 2) {
+  } else if (computerChoice === 2) {
     return "Paper";
   } else {
     return "Scissors";
@@ -13,25 +16,25 @@ function computerPlay() {
 }
 
 function playRound(playerChoice, computerChoice) {
-  if (playerChoice === "Rock" && computerChoice === "Rock") {
+  if (playerChoice === computerChoice) {
     return "Draw";
   } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-    return "You Win!";
+    return `You Win! You had ${playerChoice} and the Computer had ${computerChoice}`;
   } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-    return "You Win!";
+    return `You Win! You had ${playerChoice} and the Computer had ${computerChoice}`;
   } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-    return "You Win!";
+    return `You Win! You had ${playerChoice} and the Computer had ${computerChoice}`;
   } else {
-    return "The Computer Wins!";
+    return `You Lose! You had ${playerChoice} and the Computer had ${computerChoice}`;
   }
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
     let computerChoice = computerPlay();
+    let playerChoice = playerPlay();
     console.log(playRound(playerChoice, computerChoice));
   }
 }
 
 game();
-
